3459540744afbcb0af52ac1466ad0bcd053f893f
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 02:44:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var katzDeli = [];
 
-function takeANumber(katzDeliLine, name) {
-  katzDeliLine.push(name)
-  var position = katzDeliLine.length
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name)
+  var position = katzDeli.length
   return `Welcome, ${name}. You are number ${position} in line.`
 }
 
 function nowServing() {
-  if (katzDeliLine.length === 0) {
+  if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
     return `Now serving ${name}`
