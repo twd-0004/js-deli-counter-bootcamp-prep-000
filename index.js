2a4f229d951f652798aeced595edcfa8d3c8297e
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 02:35:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  for (let i = 1; i < katzDeliLine.length; i++) {
-    if (katzDeliLine[i-1] === name) {
-      break;
-    }
-  }
-  return i
+  var position = katzDeliLine.length
+  return position
 }
