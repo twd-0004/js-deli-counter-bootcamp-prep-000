369c1c40e039cf5e3817b5f26369eea68f454d12
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 02:38:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   var position = katzDeliLine.length
-  return position
+  return `Welcome, ${name}. You are number ${position} in line.`
+  
 }
