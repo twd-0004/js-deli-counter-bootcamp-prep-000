29fd25b10e463f04272eb056ca96985f72df82d3
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 02:46:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function takeANumber(katzDeli, name) {
   return `Welcome, ${name}. You are number ${position} in line.`
 }
 
-function nowServing() {
-  if (katzDeli.length === 0) {
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
     return `Currently serving ${name}.`
