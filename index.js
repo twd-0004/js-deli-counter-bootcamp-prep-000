4fa961dee149efc4bf3c5f6a796e59feea165e0b
Tyler Dreher <drehertyler@gmail.com>
2018-04-29 02:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!"
   } else {
-    return `Currently serving ${katzDeliLine[0]}.`
+    var serving = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${serving}.`
   }
 }
